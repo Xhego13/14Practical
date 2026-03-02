3a63e09994b787fb78efd87eb286d794e6043134
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-03-02 15:26:53 +0200

    Implement Entry class for chained hash table
    Added Entry class to represent key-value pairs in the hash table.

--- chainedHash.java
+++ chainedHash.java
@@ -1,4 +1,16 @@
 public class chainedHash{
-  
+  private static class Entry {
+        String key;
+        String value;
+     
+        Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    private LinkedList<Entry>[] table; 
+       int m;
+       int N; 
+
   
 }
