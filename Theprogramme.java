18a4a36c69c81cec25e1def69d159f0f47d2ee7f
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-03-02 15:59:16 +0200

    generation and shuffling of key-value pairs

--- Theprogramme.java
+++ Theprogramme.java
@@ -2,7 +2,24 @@
 import java.lang.Math.*;   import java.io.*;   import java.text.*;
 
 public class timeMethods{
-public static int N =1500;
+int N = 1 << 20;
+int[] keys = new int[N];
+for (int i = 0; i < N; i++) {
+    keys[i] = i;
+}
+List<Integer> keyList = new ArrayList<>();
+for (int k : keys) keyList.add(k);
+Collections.shuffle(keyList);
+
+String[][] keyValuePairs = new String[N][2];
+for (int i = 0; i < N; i++) {
+    keyValuePairs[i][0] = String.valueOf(keyList.get(i));
+    keyValuePairs[i][1] = String.valueOf(i + 1);         
+}
+int usableSize = 950_000;
+String[][] usablePairs = Arrays.copyOfRange(keyValuePairs, 0, usableSize);
+
+
 public static void main(String args[]){
 
 DecimalFormat twoD = new DecimalFormat("0.00");
