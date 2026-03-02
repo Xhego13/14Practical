b07b0a632cec279284000a6e0bd1dbb5ebaed572
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-03-02 15:09:36 +0200

    created openhash constructor
    Implemented a hash table with separate chaining using linked lists for collision handling.

--- openHash.java
+++ openHash.java
@@ -3,5 +3,45 @@ public class openHash{
         String key;
         String value;
      
-  
+    Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+ }
+
+    private LinkedList<Entry>[] table; 
+    private int m; ze
+    private int N; 
+
+    // Constructor
+    public openHash(int m) {
+        this.m = m;
+        this.N = 0;
+        table = new LinkedList[m];
+        for (int i = 0; i < m; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    
+    public int hash(String key) {
+        int h = key.hashCode();
+        return h+(N%m)
+    }
+
+    
+    public void insert(String key, String value) {
+        int index = hash(key);
+        for (Entry e : table[index]) {
+            if (e.key.equals(key)) {
+                e.value = value;
+                return;
+            }
+        }
+        table[index].add(new Entry(key, value));
+        N++;
+    }
+
+
 }
